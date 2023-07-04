cbf895e78e4bc026a411ad71544a153bcb39435d
Richards Cedeño <chards.rjcov@gmail.com>
2023-07-03 23:15:47 -0500

    Modificando detect.py para el envio de notificación en tiempo real

--- detect.py
+++ detect.py
@@ -1,6 +1,7 @@
 import cv2
 from ultralytics import YOLO
 import imutils
+import validate_and_send as send
 
 model = YOLO('./weights/best50.pt')
 
@@ -17,9 +18,20 @@ while cap.isOpened():
     if success:
         results = model(frame, conf=0.8, save_crop=True)
 
+        # print(f'Resultado conf: {results[0].boxes.conf}')
+        conf = results[0].boxes.conf
+        # print(f'conf save: {conf}')
+        # print(type(conf))
+
         plot_frame = results[0].plot()
 
         cv2.imshow("YOLOv8", plot_frame)
+
+        if conf.nelement() != 0:
+            print(f'Tiene datos: {conf.nelement()}')
+            send.send()
+        else:
+            print(f'Está vacio: {conf.nelement()}')
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
