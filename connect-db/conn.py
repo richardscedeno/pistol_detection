8094307dec300fede322788b6709aebe9628652a
Richards Cedeño <chards.rjcov@gmail.com>
2023-07-09 21:18:51 -0500

    Cambiando la estructura de conexion a la bd en una clase

--- connect-db/conn.py
+++ connect-db/conn.py
@@ -1,12 +1,16 @@
 import mysql.connector
 from decouple import config
 
-conn = mysql.connector.connect(
-    user = config('MYSQL_USER'),
-    password = config('MYSQL_PASSWORD'),
-    host = config('MYSQL_HOST'),
-    database = config('MYSQL_DB'),
-    port = config('MYSQL_PORT')
-)
-
-print(conn)
+# Creamos el objeto de acceso a los datos
+class DAO():
+    def __init__(self):
+        try:
+            self.conn = mysql.connector.connect(
+                host = config('MYSQL_HOST'),
+                port = config('MYSQL_PORT'),
+                user = config('MYSQL_USER'),
+                password = config('MYSQL_PASSWORD'),
+                database = config('MYSQL_DB')
+            )
+        except Exception as e:
+            print(e)
